998d0e4aa4db1f1bed9f6ad5ba93aee7a986cf05
Rana Sharjeel <ranasharjeel01@yahoo.com>
2024-06-15 21:20:05 +0500

    payment

--- src/components/home/paymentFormsteps.jsx
+++ src/components/home/paymentFormsteps.jsx
@@ -1,8 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Checkout from "../payment/stripeCheckout";
+import { bookingMail, createPaymentIntent } from "../../api/actions";
+import { toast } from "react-toastify";
+import { parseError } from "../../utils";
 
-const PaymentFormSteps = () => {
+const PaymentFormSteps = ({ currency, amount, isOpen }) => {
+  const [loading, setLoading] = useState(false);
   const [currentStep, setCurrentStep] = useState(1);
+  const [intent, setIntent] = useState(null);
+
+  const [data, setData] = useState({
+    email: "",
+    firstName: "",
+    lastName: "",
+    companyName: "",
+    phone: "",
+    address: "",
+    city: "",
+    postalCode: "",
+  });
+
+  const onChange = (label, value) => {
+    setData({
+      ...data,
+      [label]: value,
+    });
+  };
+
+  const resetFormDetails = () => {
+    setData({
+      ...data,
+      email: "",
+      firstName: "",
+      lastName: "",
+      companyName: "",
+      phone: "",
+      address: "",
+      city: "",
+      postalCode: "",
+    });
+  };
 
   const handleNext = () => {
     if (currentStep < 4) {
@@ -10,11 +47,40 @@ const PaymentFormSteps = () => {
     }
   };
 
-  const handlePrevious = () => {
-    if (currentStep > 1) {
-      setCurrentStep(currentStep - 1);
-    }
+  const onNext = (e) => {
+    e.preventDefault();
+    handleNext();
   };
+
+  const onPaymentSuccess = (payment) => {
+    setLoading(true);
+    bookingMail({ ...data, paymentId: payment.id })
+      .then(() => {
+        setLoading(false);
+        toast.success("Booked Successfully");
+        handleNext();
+      })
+      .catch((error) => {
+        setLoading(false);
+        toast.error(parseError(error));
+      });
+  };
+
+  const createIntent = () => {
+    createPaymentIntent({ currency, amount })
+      .then((response) => {
+        setIntent(response.data);
+      })
+      .catch((error) => {
+        toast.error(parseError(error));
+      });
+  };
+
+  useEffect(() => {
+    resetFormDetails();
+    createIntent();
+  }, [currency, amount, isOpen]);
+
   return (
     <div className="form-step">
       <h1>Complete your booking</h1>
@@ -39,35 +105,112 @@ const PaymentFormSteps = () => {
             </li>
           </ul>
         </div>
+
         <div className="col">
           {currentStep === 1 && (
-            <div className="form-group">
-              <label htmlFor="email">Enter your Email to get started</label>
-              <input type="email" placeholder="Enter your Email here" />
-              <button onClick={handleNext}>Next</button>
-            </div>
+            <form onSubmit={onNext}>
+              <div className="form-group">
+                <label htmlFor="email">Enter your Email to get started</label>
+                <input
+                  type="email"
+                  placeholder="Enter your Email here"
+                  required
+                  value={data.email}
+                  onChange={(e) => onChange("email", e.currentTarget.value)}
+                />
+                <button type="submit">Next</button>
+              </div>
+            </form>
           )}
+
           {currentStep === 2 && (
-            <div className="form-group">
-              <label htmlFor="name">Enter your name</label>
-              <div className="form-group-wrapper">
-                <input type="text" placeholder="First Name" />
-                <input type="text" placeholder="Last Name" />
-                <input type="text" placeholder="Company (optional)" />
-                <input type="text" placeholder="Phone No." />
+            <form onSubmit={onNext}>
+              <div className="form-group">
+                <label htmlFor="name">Enter your name</label>
+                <div className="form-group-wrapper">
+                  <input
+                    type="text"
+                    placeholder="First Name"
+                    required
+                    value={data.firstName}
+                    onChange={(e) =>
+                      onChange("firstName", e.currentTarget.value)
+                    }
+                  />
+
+                  <input
+                    type="text"
+                    placeholder="Last Name"
+                    required
+                    value={data.lastName}
+                    onChange={(e) =>
+                      onChange("lastName", e.currentTarget.value)
+                    }
+                  />
+
+                  <input
+                    type="text"
+                    placeholder="Company (optional)"
+                    value={data.companyName}
+                    onChange={(e) =>
+                      onChange("companyName", e.currentTarget.value)
+                    }
+                  />
+
+                  <input
+                    type="text"
+                    placeholder="Phone No."
+                    required
+                    value={data.phone}
+                    onChange={(e) => onChange("phone", e.currentTarget.value)}
+                  />
+                </div>
+
+                <label className="marginTop" htmlFor="address">
+                  Enter your address
+                </label>
+
+                <input
+                  type="text"
+                  placeholder="Address"
+                  required
+                  value={data.address}
+                  onChange={(e) => onChange("address", e.currentTarget.value)}
+                />
+
+                <div className="form-group-wrapper">
+                  <input
+                    type="text"
+                    placeholder="City"
+                    required
+                    value={data.city}
+                    onChange={(e) => onChange("city", e.currentTarget.value)}
+                  />
+
+                  <input
+                    type="text"
+                    placeholder="Postal Code"
+                    required
+                    value={data.postalCode}
+                    onChange={(e) =>
+                      onChange("postalCode", e.currentTarget.value)
+                    }
+                  />
+                </div>
+
+                <button type="submit">Next</button>
               </div>
-              <label className="marginTop" htmlFor="address">
-                Enter your address
-              </label>
-              <input type="text" placeholder="Address" />
-              <div className="form-group-wrapper">
-                <input type="text" placeholder="City" />
-                <input type="text" placeholder="Postal Code" />
-              </div>
-              <button onClick={handleNext}>Next</button>
-            </div>
+            </form>
           )}
-          {currentStep === 3 && <Checkout amount={100} />}
+
+          {currentStep === 3 && (
+            <Checkout
+              intent={intent}
+              loading={loading}
+              onSuccess={onPaymentSuccess}
+            />
+          )}
+
           {currentStep === 4 && (
             <div className="form-group">
               <label htmlFor="name">Choose your Payment Method</label>
--- src/components/payment/stripeCheckout.js
+++ src/components/payment/stripeCheckout.js
@@ -1,63 +1,73 @@
-import React, { useMemo } from "react";
+import React from "react";
 import configs from "../../configs";
 import {
-  useStripe,
-  useElements,
   Elements,
   ElementsConsumer,
   PaymentElement,
 } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import { toast } from "react-toastify";
 
 const stripePromise = loadStripe(configs.stripePublicKey);
 
-const CheckoutForm = ({ stripe, elements }) => {
-  //   const stripe = useStripe();
-  //   const elements = useElements();
-
+const CheckoutForm = ({ stripe, loading, elements, onSuccess }) => {
   const handleSubmit = async (event) => {
     event.preventDefault();
-    if (!stripe || !elements || !stripePromise) return;
-    debugger;
-    const result = await stripe.confirmPayment({
-      //`Elements` instance that was used to create the Payment Element
+    if (!stripe || !elements) return;
+    const { error, paymentIntent } = await stripe.confirmPayment({
       elements,
-      confirmParams: {
-        return_url: "https://example.com/order/123/complete",
-      },
+      redirect: "if_required",
+      //   confirmParams: {
+      //     return_url: "https://example.com/order/123/complete",
+      //   },
     });
-    debugger;
 
-    if (result.error) {
-      // Show error to your customer (for example, payment details incomplete)
-      console.log(result.error.message);
+    if (error) {
+      toast.error(error.message);
     } else {
-      // Your customer will be redirected to your `return_url`. For some payment
-      // methods like iDEAL, your customer will be redirected to an intermediate
-      // site first to authorize the payment, then redirected to the `return_url`.
+      if (paymentIntent.status === "succeeded") {
+        onSuccess(paymentIntent);
+        if (
+          paymentIntent.next_action &&
+          paymentIntent.next_action.type === "redirect_to_url"
+        ) {
+          window.location.replace(
+            paymentIntent.next_action.redirect_to_url.url
+          );
+        }
+        //  else {
+        //   onSuccess(paymentIntent);
+        // }
+      }
     }
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <PaymentElement />
-      <button>Submit</button>
+      <div className="form-group">
+        <button>{loading ? "Loading..." : "Pay"}</button>
+      </div>
     </form>
   );
 };
 
-const Checkout = ({ currency = "usd", amount }) => {
-  const options = useMemo(() => ({
-    mode: "payment",
-    currency,
-    amount,
-  }));
-
+const Checkout = ({ intent, loading, onSuccess }) => {
   return (
-    <Elements stripe={stripePromise} options={options}>
+    <Elements
+      stripe={stripePromise}
+      options={{
+        clientSecret: intent.clientSecret,
+      }}
+    >
       <ElementsConsumer>
         {({ stripe, elements }) => (
-          <CheckoutForm stripe={stripe} elements={elements} />
+          <CheckoutForm
+            loading={loading}
+            stripe={stripe}
+            elements={elements}
+            onSuccess={onSuccess}
+          />
         )}
       </ElementsConsumer>
     </Elements>
